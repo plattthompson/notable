267612fc1b9f0eb8f5f9e63150288540e677d633
Platt Thompson <plattthompsonprogramming@gmail.com>
2019-07-17 17:07:15 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Note from './components/Note.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+				<Note />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Note extends Component {
+	render() {
+		return (
+			<div>
+				<textarea placeholder="Create your note here"></textarea>
+			</div>
+		);
+	}
+}
+
+export default Note;
