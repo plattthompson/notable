6e692610a8a9c9d18171c49c3fdf99b54e18db57
Platt Thompson <plattthompsonprogramming@gmail.com>
2019-07-20 13:02:38 -0700

    changed state data format

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import Header from './components/Header.js';
 import Add from './components/Add.js';
 import Delete from './components/Delete.js';
 import Save from './components/Save.js'
-import Menu from './components/Menu.js';
+import Titles from './components/Titles.js';
 import Note from './components/Note.js';
 
 class App extends Component {
 	state = {
-		id: 1,
-		title: "",
-		text: ""
+		1: {
+			id: 1,
+			title: "",
+			text: ""
+		}
+	}
+
+	selectNote = () => {
+
 	}
 
 	saveText = (event) => {
 		this.setState({ text: event.target.value, });
 	}
 
-	newNote = () => {
+	addNote = () => {
 		// Prepend title, empty state
 	}
 
@@ -30,44 +36,22 @@ class App extends Component {
 		// Empty string, delete node, send delete request to back-end
 	}
 
+	// this.state.map
+
 	render() {
 		return (
 			<div>
 				<Header />
 				<div className="app-container background">
-					<Add />
+					<Add addNote={this.addNote} />
 					<Save saveChanges={this.saveChanges} />
-					<Delete />
-					<Menu />
+					<Delete deleteNote={this.deleteNote} />
+					<Titles selectNote={this.selectNote} />
 					<Note saveText={this.saveText} />
 				</div>
 			</div>
 		);
 	}
 }
-/*
-<div>
-				<Title className="title" />
-				<div className="app-container background">
-					<Grid container justify="center" alignItems="center">
-						<Grid item sm={4} align="center">
-							<Add className="add"/>
-						</Grid>
-						<Grid item sm={4} align="center">
-							<SaveNote saveChanges={this.saveChanges} className="save-note"/>
-						</Grid>
-						<Grid item sm={4} align="center">
-							<Trash className="trash"/>
-						</Grid>
-						<Grid item sm={6}>
-							<Menu className="menu"/>
-						</Grid>
-						<Grid item sm={6}>
-							<Note saveText={this.saveText} className="note"/>
-						</Grid>
-					</Grid>
-				</div>
-			</div>
-*/
 
 export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Titles extends Component {
+	render() {
+		return (
+			<div className="menu">
+				<ul className="list-items">
+					<li>
+						<input className="title-box"/>
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Titles;
